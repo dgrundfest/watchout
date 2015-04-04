1d7f7376dfecf0ae4f512ea34b1e5305e0f93eb3
dgrundfest <dave4@grundfest.com>
2015-04-04 11:52:48 -0700

    Added Broccili and cleaned up

--- watchout.js
+++ watchout.js
@@ -40,27 +40,30 @@ var enemies = gameBoard.selectAll('image.enemy')
 
 
 var drag = d3.behavior.drag()
-             .on('dragstart', function() { players.style({'fill' : 'green', 'stroke': 'green'}); })
-             .on('drag', function() { players.attr('cx', d3.event.x)
-                                            .attr('cy', d3.event.y); })
-             .on('dragend', function() { players.style({'fill': 'red', 'stroke': 'red'});});
+             // .on('dragstart', function() { players.style({'fill' : 'green', 'stroke': 'green'}); })
+             .on('drag', function() { players.attr('x', d3.event.x)
+                                            .attr('y', d3.event.y); })
+             // .on('dragend', function() { players.style({'fill': 'red', 'stroke': 'red'});});
 
 
  var player = [{
-   'cx' : axes.x(50),
-   'cy' : axes.y(50),
+   'x' : axes.x(50),
+   'y' : axes.y(50),
 }]
 
- var players = gameBoard.selectAll('circle.player')
+ var players = gameBoard.selectAll('image.player')
                     .data(player);
 
  players.enter()
-  .append('svg:circle')
+  .append('svg:image')
    .attr('class', 'player')
-    .attr('cx', function(d){ return d.cx})
-   .attr('cy', function(d){ return d.cy})
-   .attr('r', 10)
-    .style({'fill': 'red','stroke':'red'})
+    .attr('x', function(d){ return d.x})
+   .attr('y', function(d){ return d.y})
+   // .attr('r', 10)
+   //  .style({'fill': 'red','stroke':'red'})
+   .attr("xlink:href", "broccoli.png")
+      .attr('width', 60)
+      .attr('height',60)
     .call(drag)
  players.exit()
    .remove()
@@ -76,13 +79,13 @@ enemies.transition()
 
 
 var checkCollisionTween = function(){
-var radiusSum = 25;
+var radiusSum = 30;
 var hasCollided = false;
 return function(){
     var eX = d3.select(this).attr("x");
     var eY = d3.select(this).attr("y");
-    var pX = d3.selectAll("circle.player").attr("cx");
-    var pY = d3.selectAll("circle.player").attr("cy");
+    var pX = d3.selectAll("image.player").attr("x");
+    var pY = d3.selectAll("image.player").attr("y");
     var xDiff=eX - pX;
     var yDiff=eY - pY;
     var dist = Math.sqrt(Math.pow(xDiff, 2) + Math.pow(yDiff, 2));
@@ -95,7 +98,7 @@ return function(){
 
 var upDateScore = function(){
   window.timeElapsed++;
-  var score = Math.trunc(window.timeElapsed / 100 - window.collisions);
+  var score = Math.trunc(window.timeElapsed / 1000 - window.collisions);
   if(score > window.highScore){
     window.highScore = score
   }
